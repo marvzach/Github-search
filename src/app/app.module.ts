680363ede15d28d0f87ad349cd8bddd260cd552a
Marvin Zachary <acerzach@gmail.com>
2019-03-31 21:42:35 +0300

    Added imports to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import {FormsModule} from '@angular/forms';
-// import {HttpClientModule} from '@angular/common/http';
-// import { DetailspageService} from './geet/detailspage.service';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { DetailspageService} from './geet/detailspage.service';
 
 
-// import { AppComponent } from './app.component';
-// import { DetailspageComponent } from './detailspage/detailspage.component';
-// import { InformationComponent } from './information/information.component';
-// import { ReposComponent } from './repos/repos.component';
+import { AppComponent } from './app.component';
+import { DetailspageComponent } from './detailspage/detailspage.component';
+import { InformationComponent } from './information/information.component';
+import { ReposComponent } from './repos/repos.component';
 
 
 // @NgModule({
