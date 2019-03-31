51506d0a7b86e188e9bff74674d70ba288bdb3bc
Marvin Zachary <acerzach@gmail.com>
2019-03-31 21:49:50 +0300

    Added getprofilerepo and sourced the github api

--- src/app/geet/detailspage.service.ts
+++ src/app/geet/detailspage.service.ts
@@ -17,13 +17,13 @@ export class DetailspageService {
    // tslint:disable-next-line:max-line-length
    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
 
-//  }
-//  getprofileRepos() {
-// // tslint:disable-next-line:max-line-length
-// return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
+ }
+ getprofileRepos() {
+// tslint:disable-next-line:max-line-length
+return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret);
 
 
-//    }
+   }
 
 //  updateprofile(username: string) {
 //    this.username = username;
