44a1dfef0fc1aad28c8a88c5f89e7622fb157b90
Marvin Zachary <acerzach@gmail.com>
2019-03-31 21:46:24 +0300

    Added the username,clientid and clientsecret

--- src/app/geet/detailspage.service.ts
+++ src/app/geet/detailspage.service.ts
@@ -4,9 +4,9 @@ import {HttpClient} from '@angular/common/http';
 @Injectable()
 export class DetailspageService {
 
-  // private username: string;
-  // private clientid = '97ad8e14c7e6a33245c6';
-  // private clientsecret = '375f816175ae38c0b1fed8c03a6de70aa36ad2d1';
+  private username: string;
+  private clientid = '97ad8e14c7e6a33245c6';
+  private clientsecret = '375f816175ae38c0b1fed8c03a6de70aa36ad2d1';
 
   // constructor(private http: HttpClient) {
   //     console.log('Ready for display');
